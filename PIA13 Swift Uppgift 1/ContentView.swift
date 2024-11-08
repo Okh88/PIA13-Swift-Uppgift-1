e2c9ae015edc617db5fd837284e50ddf44532db8
Okh88 <orwah.khadrow@gmail.com>
2024-11-08 14:06:44 +0100

    last

--- PIA13 Swift Uppgift 1/ContentView.swift
+++ PIA13 Swift Uppgift 1/ContentView.swift
@@ -9,12 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            // Full-screen background color
-            Color.blue
-                .edgesIgnoringSafeArea(.all) // Makes the color fill the entire screen
-            
-            VStack(spacing: 0) {
+        
+VStack(spacing: 0) {
                 // Top Row (Red, Green, Yellow)
                 HStack(alignment: .top, spacing: 0) {
                     Color.red
@@ -36,25 +32,38 @@ struct ContentView: View {
                     .padding([.top, .leading, .trailing], 3.0)
                 
                 // Blue Section with White Square
+    
                 ZStack {
                     Color.white
                         .frame(width: 100, height: 100)
                         .padding(.bottom, 422.0)
-                }
-                .frame(height: 550)
+               
+                .frame(height: 650)
                 // Bottom Row (Red background with Black Square on bottom right)
+                    Spacer()
+
                 ZStack(alignment: .bottomTrailing) {
                     Color.red
-                    
+                        .frame(maxWidth: .infinity,maxHeight: 100)
+                        .position(x:200,y:615)
+                    HStack{
+                        Spacer()
+                  
                     Color.black
                         .frame(width: 50, height: 50)
                         .padding(20)
                 }
-                .frame(height: 90)
+                .frame(height: 66)
+                }
             }
         }
+        
+     .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color.cyan)
+      .ignoresSafeArea()
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
